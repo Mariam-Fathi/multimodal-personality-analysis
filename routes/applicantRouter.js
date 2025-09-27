12e2552bf7931bad57bc6b3a179f377ded1d7eab
Mariam Fathi <mariam.f.siam@gmail.com>
2025-09-27 12:14:27 +0300

    optimize applicant router structure and middleware

--- routes/applicantRouter.js
+++ routes/applicantRouter.js
@@ -1,44 +1,40 @@
-const express = require("express"),
-  applicantRouter = express.Router();
-
+const express = require("express");
 const fileUpload = require("express-fileupload");
-
 const path = require("path");
-const directoryPath = path.dirname(require.main.filename);
-applicantRouter.use(express.static(path.join(directoryPath, "statics")));
 
-const currentUser = require("../middlewares/current-user"),
-  requireAuth = require("../middlewares/require-auth");
-const filesPayloadExists = require("../middlewares/filesPayloadExists"),
-  fileExtLimiter = require("../middlewares/fileExtLimiter"),
-  fileSizeLimiter = require("../middlewares/fileSizeLimiter");
+const currentUser = require("../middlewares/current-user");
+const requireAuth = require("../middlewares/require-auth");
+const filesPayloadExists = require("../middlewares/filesPayloadExists");
+const fileExtLimiter = require("../middlewares/fileExtLimiter");
+const fileSizeLimiter = require("../middlewares/fileSizeLimiter");
+const { getMyInfo, uploadVideo } = require("../controllers/applicantController");
 
-const {
-  getMyInfo,
-  uploadVideo,
-} = require("../controllers/applicantController");
+const applicantRouter = express.Router();
+
+const staticPath = path.join(path.dirname(require.main.filename), "statics");
+applicantRouter.use(express.static(staticPath));
 
 applicantRouter.get("/upload-video/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/uploadVideo.html"));
+    res.sendFile(path.join(__dirname, "../views/uploadVideo.html"));
 });
+
 applicantRouter.get("/waiting/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/waiting.html"));
+    res.sendFile(path.join(__dirname, "../views/waiting.html"));
 });
+
 applicantRouter.get("/personality-analysis/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/analysis.html"));
+    res.sendFile(path.join(__dirname, "../views/analysis.html"));
 });
 
 applicantRouter.get("/get-my-info", currentUser, requireAuth, getMyInfo);
 
 applicantRouter.post(
-  "/upload-video",
-  fileUpload({ createParentPath: true }),
-  filesPayloadExists,
-  fileExtLimiter([".mp4", ".mov", ".wmv"]),
-  fileSizeLimiter,
-  uploadVideo
+    "/upload-video",
+    fileUpload({ createParentPath: true }),
+    filesPayloadExists,
+    fileExtLimiter([".mp4", ".mov", ".wmv"]),
+    fileSizeLimiter,
+    uploadVideo
 );
 
-module.exports = {
-  applicantRouter,
-};
+module.exports = { applicantRouter };
